da9bd8984a6cb5eb05111548eeae2d0536a50a1f
iniix <iniix@proton.me>
2026-07-05 22:43:00 -0400

    added new reviews and photos

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -13,5 +13,20 @@ export const reviews = [
         name: "Beverly C",
         text: "Think the world of Tammy!!! Always gives a very relaxing pedicure!!!!!!",
         rating: "★★★★★"
+    },
+    {
+        name: "Carol C",
+        text: "Thank you Tammy, you did a beautiful job.",
+        rating: "★★★★★"
+    },
+    {
+        name: "Lisa S",
+        text: "Love my nails! It is always a great experience!!",
+        rating: "★★★★★"
+    },
+    {
+        name: "Donna S",
+        text: "Always a great experience and always satisfied.",
+        rating: "★★★★★"
     }
 ];
